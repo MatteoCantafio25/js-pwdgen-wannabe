c16a4fefbacee26ce620ddc2824827df8bdc643e
MatteoCantafio25 <mattyteo004@gmail.com>
2023-12-17 17:09:20 +0100

    created the script for the task

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
-console.log("JS OK");
 //---------------------------------//
+// MI ASSICURO CHE IL FOGLIO JS COMUNICHI CON IL FOGLIO HTML
 
+console.log("JS OK");
 
+//---------------------------------//
+// CREO UNA COSTANTE FIRSTNAME A CUI DARA' VALORE L'UTENTE
+
+const firstName = prompt("Come ti chiami?");
+
+console.log(firstName);
+
+//---------------------------------//
+// CREO UNA COSTANTE LASTNAME
+
+const lasttName = prompt("Qual è il tuo cognome?");
+
+console.log(lasttName);
+
+//---------------------------------//
+// CREO UNA COSTANTE FAVCOLOR
+
+const favColor = prompt("Qual è il tuo colore preferito?");
+
+console.log(favColor);
+
+//---------------------------------//
+// CREO UNA COSTANTE NUMBER PER METTERLO ALLA FINE DELLA PASSWORD SENZA L'AIUTO DELL'UTENTE
+
+const number = 23;
+
+console.log(number);
+
+//---------------------------------//
+// PRENDO DAL MIO FOGLIO HTML L'ELEMENTO CON L'ID PASSWORD E LO CHIAMO ELEMENT
+
+const element = document.getElementById("password");
+
+console.log(element);
+
+//---------------------------------//
+// CREO UIN MESSAGGIO DA FAR APPARIRE NELL'ELEMENT
+
+const message = "La tua password è:" + " " + firstName + lasttName + favColor + number;
+
+console.log(message);
+
+//---------------------------------//
+// STAMPO IN PAGINA L'ELEMENTO CON IL MESSAGGIO
+
+element.innerText = message;
+
+//---------------------------------//
+// STAMPO IN CONSOLE IL RISULTATO FINALE DELLA PASSWORD
+
+const sum = firstName + lasttName + favColor + number;
+
+console.log(sum);
+
+//---------------------------------//
 
 
